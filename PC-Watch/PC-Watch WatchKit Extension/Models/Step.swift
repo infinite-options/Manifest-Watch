5e5013f4d3891487ad572730b0b5d8e19be7715d
jtlystudent0 <jonathantly.01@gmail.com>
2021-11-11 09:35:06 -0800

    added step model

---
+++ PC-Watch/PC-Watch WatchKit Extension/Models/Step.swift
@@ -0,0 +1,37 @@
+//
+//  Step.swift
+//  PC-Watch WatchKit Extension
+//
+//  Created by Jonathan Ly on 11/11/21.
+//  Copyright © 2021 Infinite Options. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Steps
+struct StepsResponse: Codable {
+    let result: [Steps]
+    let message: String
+}
+
+// MARK: - Steps
+struct Steps: Codable {
+    let isUniqueID, isTitle, atID: String
+    let isSequence: Int
+    let isAvailable, isComplete, isInProgress, isTimed: String
+    let isPhoto: String
+    let isExpectedCompletionTime: String
+
+    enum CodingKeys: String, CodingKey {
+        case isUniqueID = "is_unique_id"
+        case isTitle = "is_title"
+        case atID = "at_id"
+        case isSequence = "is_sequence"
+        case isAvailable = "is_available"
+        case isComplete = "is_complete"
+        case isInProgress = "is_in_progress"
+        case isTimed = "is_timed"
+        case isPhoto = "is_photo"
+        case isExpectedCompletionTime = "is_expected_completion_time"
+    }
+}
